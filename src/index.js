7f94e9042e1dcb0cf1fd90812c6509fd6fc400ac
Bot-Casey <casey@stadick.tech>
2021-02-21 01:27:33 -0800

    Init
    Renders Google Maps
    Custom Styling via mapStyles.js
        - Not available without using google api key in env.local
    Adding Markers onMouseClick
    Infobox drawn on selected marker
    Homebutton

--- src/index.js
+++ src/index.js
@@ -1,8 +1,109 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM, { render } from 'react-dom';
+import mapStyles from "./mapStyles";
+import {
+  GoogleMap,
+  useLoadScript,
+  LoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api"
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const mapContainerStyle = {
+  width: "100vw",
+  height: "100vh",
+  padding: '0px 0px 0px 0px',
+}
+
+const center = {
+  lat: 46.831073,
+  lng: -100.773183
+}
+
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+};
+
+export default function App() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  });
+
+  const [markers, setMarkers] = React.useState([]);
+  const [selected, setSelected] = React.useState(null);
+  const mapRef = React.useRef();
+
+  const onMapClick = React.useCallback((event) => {
+    setSelected(null);
+    setMarkers(current => [
+      ...current,
+      {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+        time: new Date(),
+      }])
+  }, []);
+  
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+    
+  }, []);
+
+  const panTo = React.useCallback(({ lat, lng, zoom }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(zoom);
+  }, []);
+
+  if (loadError) return "Error loading map";
+  if (!isLoaded) return "Loading Maps";
+
+  return (
+    <div>
+      <h4 className='waterMark'>Casey Stadick</h4>
+      <Locate panTo={panTo} />
+      <GoogleMap
+        options={options}
+        mapContainerStyle={mapContainerStyle}
+        zoom={13}
+        center={center}
+        onClick={onMapClick}
+        onLoad={onMapLoad}>
+        {markers.map(marker =>
+          <Marker
+            key={marker.time.toISOString()}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+          />
+        )}
+
+        {selected ? (
+          <InfoWindow position={{ lat: selected.lat, lng: selected.lng }} onCloseClick={() => setSelected(null)}>
+            <div>
+              <h2>Stuff</h2>
+            </div>
+          </InfoWindow>
+        ) : null}
+
+      </GoogleMap>
+    </div>
+  );
+
+}
+
+function Locate({ panTo }) {
+  return (
+    <button className="locate" onClick={() => navigator.geolocation.getCurrentPosition(
+      (position) => panTo({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+        zoom: 15}),
+      () => null)}><img className="homeImage" src="home.png"/></button>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +111,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
