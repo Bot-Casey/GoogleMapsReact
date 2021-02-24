b467e0a47194ba4824b3e1529cd621a534426887
Bot-Casey <casey@stadick.tech>
2021-02-24 09:47:41 -0600

    Added ISS Plotting
     - ISS will now be plotted on the map and whenever you click the 'world' icon
     - Info windows now display lat and lng values rounded to two significant digits
     - Refactored code to use ' instead of "
     - Reorganized main app() render to make more sense

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from "react";
+import React from 'react';
 import ReactDOM, { render } from 'react-dom';
-import mapStyles from "./mapStyles";
+import mapStyles from './mapStyles';
 import {
   GoogleMap,
   useLoadScript,
   Marker,
   InfoWindow,
-} from "@react-google-maps/api"
+} from '@react-google-maps/api'
 import './index.css';
 
 require('dotenv').config();
 console.log(process.env.REACT_APP_GOOGLE_MAPS_KEY);
 
 const mapContainerStyle = {
-  width: "100vw",
-  height: "100vh",
+  width: '100vw',
+  height: '100vh',
   padding: '0px 0px 0px 0px',
 }
 
@@ -47,10 +47,34 @@ export default function App() {
         time: new Date(),
       }])
   }, []);
-  
+
+  const plotISS = async () => {
+    let payload = null;
+
+    await fetch('https://api.wheretheiss.at/v1/satellites/25544')
+      .then(response => response.json())
+      .then((data) => payload = data);
+
+    const curTime = new Date();
+
+    setMarkers( current =>  [
+      ...current,
+      {
+      lat: payload.latitude,
+      lng: payload.longitude,
+      time: curTime,
+    }]);
+  }
+
+  const panToISS = React.useCallback(({ lat, lng, zoom}) => {
+    mapRef.issMarker.panTo({ lat, lng });
+    mapRef.current.setZoom(zoom);
+  }, []);
+
   const onMapLoad = React.useCallback((map) => {
     mapRef.current = map;
-    
+    const issMarker = plotISS();
+    console.log(issMarker);
   }, []);
 
   const panTo = React.useCallback(({ lat, lng, zoom }) => {
@@ -58,13 +82,15 @@ export default function App() {
     mapRef.current.setZoom(zoom);
   }, []);
 
-  if (loadError) return "Error loading map";
-  if (!isLoaded) return "Loading Maps";
+  if (loadError) return 'Error loading map';
+  if (!isLoaded) return 'Loading Maps';
 
   return (
     <div>
-      <h4 className='waterMark'>Casey Stadick</h4>
-      <Locate panTo={panTo} />
+      <nav className='menu'>
+        <ISS plotISS={plotISS} />
+        <Locate panTo={panTo} />
+      </nav>
       <GoogleMap
         options={options}
         mapContainerStyle={mapContainerStyle}
@@ -85,12 +111,16 @@ export default function App() {
         {selected ? (
           <InfoWindow position={{ lat: selected.lat, lng: selected.lng }} onCloseClick={() => setSelected(null)}>
             <div>
-              <h2>Stuff</h2>
+              <h3>Coords:</h3>
+              <h3>
+              {Math.round(selected.lat * 100) / 100}, {Math.round(selected.lng * 100) / 100}
+              </h3>
             </div>
           </InfoWindow>
         ) : null}
 
       </GoogleMap>
+      <h4 className='waterMark'>Casey Stadick</h4>
     </div>
   );
 
@@ -98,17 +128,28 @@ export default function App() {
 
 function Locate({ panTo }) {
   return (
-    <button className="locate" onClick={() => navigator.geolocation.getCurrentPosition(
+    <button className='locate' onClick={() => navigator.geolocation.getCurrentPosition(
       (position) => panTo({
         lat: position.coords.latitude,
         lng: position.coords.longitude,
-        zoom: 15}),
-      () => null)}><img className="homeImage" src="home.png"/></button>
+        zoom: 15
+      }),
+      () => null)}><img className='locate-img' src='home.png' /></button>
+  )
+}
+
+function ISS({ plotISS }) {
+  return (
+    <button className='iss' onClick={plotISS}><img className='iss-img' src='iss.png'></img></button>
   )
 }
 
 ReactDOM.render(
   <React.StrictMode>
+    <header>
+      <title>Maps App<link href="logo.png" /></title>
+      
+    </header>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
